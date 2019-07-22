64e7388f07fc047de4fedf8e6c3825aba1e707b2
brenetic <b@brenetic.com>
2019-07-22 22:57:49 +0100

    Add ba sing se uploads s3 bucket

--- ba-sing-se/s3.tf
+++ ba-sing-se/s3.tf
@@ -14,3 +14,27 @@
     prevent_destroy = true
   }
 }
+
+ resource "aws_s3_bucket" "ba_sing_se_uploads" {
+  bucket = "${local.service}-uploads"
+  region = "${local.region}"
+
+   server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+   lifecycle {
+    prevent_destroy = true
+  }
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST", "GET", "HEAD", "DELETE"]
+    allowed_origins = ["*"]
+    max_age_seconds = 3000
+  }
+}
