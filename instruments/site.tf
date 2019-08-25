dce2897b6b56ca56c619691416ae75c53655a1e8
brenetic <b@brenetic.com>
2019-08-25 20:23:16 +0100

    Add instruments site

---
+++ instruments/site.tf
@@ -0,0 +1,44 @@
+locals {
+  service = "instruments"
+  region  = "eu-west-2"
+}
+
+provider "aws" {
+  version     = ">= 2.17.0"
+  max_retries = 1
+  region      = "eu-west-2"
+}
+
+provider "netlify" {
+  token = "${data.aws_ssm_parameter.netlify_token.value}"
+}
+
+terraform {
+  required_version = "0.12.3"
+
+   backend "s3" {
+    bucket  = "instruments-tfstate"
+    key     = "instruments.tfstate"
+    region  = "eu-west-2"
+    encrypt = true
+  }
+}
+
+data "aws_ssm_parameter" "netlify_token" {
+  name = "netlify-token"
+}
+
+resource "netlify_deploy_key" "key" {}
+
+resource "netlify_site" "instruments" {
+  name = "${local.service}"
+
+  repo {
+    repo_branch   = "master"
+    command       = "HUGO_ENV=production hugo"
+    deploy_key_id = "${netlify_deploy_key.key.id}"
+    dir           = "public"
+    provider      = "github"
+    repo_path     = "brenetic/instruments"
+  }
+}
