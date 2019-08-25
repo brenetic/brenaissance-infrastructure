364e31a3af63b5ce8283675b7aca303292ed2f6f
brenetic <b@brenetic.com>
2019-08-25 20:42:30 +0100

    Use ssm parameter store instead of encryption

--- dns/records/home-one.tf
+++ dns/records/home-one.tf
@@ -1,15 +1,9 @@
-data "aws_kms_secrets" "brenetic_netlify_ip" {
-  secret {
-    name    = "netlify_ip"
-    payload = "AQICAHiqGtTMHU0w6zM2AUzUl6QjIhh4bKxo5wwDovph0CfB/wFK3jdszbThozT6lWlk4DldAAAAazBpBgkqhkiG9w0BBwagXDBaAgEAMFUGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMSAKsDfDU7fJ3ZL+sAgEQgCgCIBS8V7z38Ymg0xFkF9/B/bGzbyh0EMPWV0YZ4z2w1C9lG1k/9BAf"
-  }
+data "aws_ssm_parameter" "brenetic_netlify_ip" {
+  name = "brenetic-netlify-ip-address"
 }
 
-data "aws_kms_secrets" "brenetic_netlify_domain" {
-  secret {
-    name    = "netlify_domain"
-    payload = "AQICAHiqGtTMHU0w6zM2AUzUl6QjIhh4bKxo5wwDovph0CfB/wFkaU08/OVDmtVSVQnbMdwcAAAAcjBwBgkqhkiG9w0BBwagYzBhAgEAMFwGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQM4wirbebStFXPtqe7AgEQgC+z8NegzszCPO+37tu6+zSoQP6jrgPNwqdoel8rFMCluYj8gMnUazsA25+z8apb6g=="
-  }
+data "aws_ssm_parameter" "brenetic_netlify_domain" {
+  name = "brenetic-netlify-host"
 }
 
 resource "aws_route53_record" "brenetic_web" {
@@ -18,7 +12,7 @@ resource "aws_route53_record" "brenetic_web" {
   type    = "A"
   ttl     = "60"
 
-  records = ["${data.aws_kms_secrets.brenetic_netlify_ip.plaintext["netlify_ip"]}"]
+  records = ["${data.aws_ssm_parameter.brenetic_netlify_ip.value}"]
 }
 
 resource "aws_route53_record" "www_brenetic_web" {
@@ -27,7 +21,7 @@ resource "aws_route53_record" "www_brenetic_web" {
   type    = "CNAME"
   ttl     = "60"
 
-  records = ["${data.aws_kms_secrets.brenetic_netlify_domain.plaintext["netlify_domain"]}"]
+  records = ["${data.aws_ssm_parameter.brenetic_netlify_domain.value}"]
 }
 
 resource "aws_route53_record" "brenetic_mx_record" {
--- home-one/site.tf
+++ home-one/site.tf
@@ -1,5 +1,31 @@
+locals {
+  service = "brenetic"
+  region  = "eu-west-2"
+}
+
+provider "aws" {
+  version     = ">= 2.17.0"
+  max_retries = 1
+  region      = "eu-west-2"
+}
+
+terraform {
+  required_version = "0.12.3"
+
+   backend "s3" {
+    bucket  = "home-one-tfstate"
+    key     = "home-one.tfstate"
+    region  = "eu-west-2"
+    encrypt = true
+  }
+}
+
 provider "netlify" {
-  token = "${data.aws_kms_secrets.netlify_token.plaintext["netlify_token"]}"
+  token = "${data.aws_ssm_parameter.netlify_token.value}"
+}
+
+data "aws_ssm_parameter" "netlify_token" {
+  name = "netlify-token"
 }
 
 resource "netlify_deploy_key" "key" {}
